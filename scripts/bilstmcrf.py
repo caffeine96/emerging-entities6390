33c50125f44859bf8db668a7d2a9a323acbde7f8
Maitrey Mehta <maitrey@redfish.cs.utah.edu>
2022-02-14 21:57:46 -0700

    Training module added

--- scripts/bilstmcrf.py
+++ scripts/bilstmcrf.py
@@ -1,7 +1,7 @@
 import torch
 
 class BiLSTMCRF(torch.nn.Module):
-    def __init__(self, data, emb_size= 1024, hidden_size = 768, dropout=0.01):
+    def __init__(self, data, emb_size= 1024, hidden_size = 768, dropout=0.01, crf_layer=False,device="cpu"):
         """ Constructor for the BiLSTM CRF module
         Inputs
         --------------
@@ -12,6 +12,8 @@ class BiLSTMCRF(torch.nn.Module):
         """
         super(BiLSTMCRF, self).__init__()
         self.emb_size = emb_size
+        self.crf_layer = crf_layer
+        self.device = device
         # Intializing the LSTM layer. Inputs are embedded character
         # vectors which will be initailized once we know the vocabulary of 
         # characters.
@@ -22,7 +24,7 @@ class BiLSTMCRF(torch.nn.Module):
         self.relu = torch.nn.ReLU() 
         self.fc2 = torch.nn.Linear(int(hidden_size/2),len(data.labels))
 
-
+        self.softmax = torch.nn.Softmax(dim=-1)
 
     def prep_tokenizer(self,data):
         """ Computes the distinct number of characters in files.
@@ -36,28 +38,81 @@ class BiLSTMCRF(torch.nn.Module):
                         if charac not in vocab:
                             vocab.append(charac)
         self.vocab = vocab
-        self.embedding_layer = torch.nn.Embedding(len(vocab),self.emb_size)
+        self.embedding_layer = torch.nn.Embedding(len(vocab),self.emb_size).to(self.device)
         
+        if not self.crf_layer:
+            count_list = [0]*len(data.labels)
+            total = 0
+            for sent_lab in data.train_data['label']:
+                for lab in sent_lab:
+                    total += 1
+                    count_list[lab] += 1
+            class_weights = torch.Tensor(list(map(lambda x : 1 - (x/total),count_list)))
+            print(f"\nClass Weights: {class_weights}\n")
+            self.loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights).to(self.device)
     
-    
+
+
     def tokenize(self, inp_data):
+        """ Breaks down words into characters
+        Inputs
+        ---------------
+        inp_data - List. List of tokenized sentences
+
+        Outputs
+        ----------------
+        inp_tok - List. List of tokenized sentences
+                    broken down character-wise 
+        inp_ind - List. List of indices which maintains
+                    information of which character belongs
+                    to which word
+        """
         inp_tok = []
         inp_ind = []
-
-        for item in inp_data['inp']:
+        
+        for item in inp_data:
             sent_idx = []
             sent_pos = []
-            for ix, word in enumerate(item):
+            ix = 0
+            for i, word in enumerate(item):
                 pos = []
                 for charac in word:
-                    sent_idx.append(charac)
+                    sent_idx.append(self.vocab.index(charac))
                     pos.append(ix)
+                    ix+=1
                 sent_pos.append(pos)
             inp_tok.append(sent_idx)
             inp_ind.append(sent_pos)
-            
-            print(inp_tok)
-            print(inp_ind)
-            exit()
+        
+        return inp_tok, inp_ind
 
+
+
+    def _mean_pool(self, inp, ixs):
+        mean_pooled_emb = None
+        for ix_set in ixs:
+            avg_pooled = torch.mean(inp[0,ix_set,:], dim=0, keepdim=True)
+            if mean_pooled_emb == None:
+                mean_pooled_emb = avg_pooled
+            else:
+                mean_pooled_emb = torch.cat((mean_pooled_emb,avg_pooled),dim=0)
+
+        return mean_pooled_emb.unsqueeze(0)
+
+
+    def compute_loss(self, data):
+        """ Compute Loss for the training sample
+        Inputs
+        ---------------
+        data- dict. Dictionary containing the training sample information
+        """
+        gold_lab = torch.Tensor(data['label']).to(torch.long).to(self.device)
+        inp = torch.Tensor(data['inp_tok']).to(torch.long).unsqueeze(0).to(self.device)
+        emb_inp = self.embedding_layer(inp)
+        lstm_out, _= self.lstm(emb_inp)
+        lstm_out_comb = self._mean_pool(lstm_out,data['inp_ind'])
+        out = self.fc2(self.relu(self.fc1(lstm_out_comb)))
+        return self.loss_fn(out.squeeze(0),gold_lab)
+        
+        
 
--- scripts/model.py
+++ scripts/model.py
@@ -1,4 +1,12 @@
+import pandas as pd
+import torch
+from tqdm import tqdm
+
 from bilstmcrf import BiLSTMCRF
+
+GPU_ID = '1'
+torch.manual_seed(42)
+device = torch.device(f"cuda:{GPU_ID}" if torch.cuda.is_available() else "cpu")
 
 class Model():
     def __init__(self, exp_name, data):
@@ -12,12 +20,38 @@ class Model():
         self.exp_name = exp_name
 
         if self.exp_name == "lstm_char":
-            self.model = BiLSTMCRF(data)
+            self.model = BiLSTMCRF(data,device=device).to(device)
             self.model.prep_tokenizer(data)
 
 
     
     
-    def train(self):
-        tr_inp_tok, tr_inp_ind = self.model.tokenize(self.data.train_data)
+    def train(self, max_ep=10, lr=0.001):
+        # Tokenize and convert words into sub-words/characters 
+        # and sub-words/characters to indices
+        self.data.train_data['inp_tok'], self.data.train_data['inp_ind'] = \
+                self.model.tokenize(self.data.train_data['inp'])
+        self.data.dev_data['inp_tok'], self.data.test_data['inp_ind'] = \
+                self.model.tokenize(self.data.dev_data['inp'])
+        self.data.test_data['inp_tok'], self.data.test_data['inp_ind'] = \
+                self.model.tokenize(self.data.test_data['inp'])
+
+        train_set   = pd.DataFrame.from_dict(self.data.train_data)
+        dev_set     = pd.DataFrame.from_dict(self.data.dev_data)
+        test_set    = pd.DataFrame.from_dict(self.data.test_data)
+       
+        optimizer = torch.optim.Adam(list(self.model.parameters()),lr=lr)
+
+        for ep in range(1,max_ep+1):
+            print(f"Epoch {ep}\n")
+            ep_tr_loss = 0
+            for train_ix, row in tqdm(train_set.iterrows()): 
+                optimizer.zero_grad()
+                loss = self.model.compute_loss(row)
+                loss.backward()
+                optimizer.step()
+                ep_tr_loss += loss.item()
+
+            print(f"Average Train Loss: {ep_tr_loss/len(train_set)}\n")
+
         
